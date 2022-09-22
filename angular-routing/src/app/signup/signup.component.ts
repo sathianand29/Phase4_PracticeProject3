7d037a657559eee1c6515e4c98a277dc95a567eb
sathianand29 <109607574+sathianand29@users.noreply.github.com>
2022-09-22 22:28:54 +0530

    Add files via upload

---
+++ angular-routing/src/app/signup/signup.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl } from '@angular/forms';
+import { Login } from '../login';
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+loginRef = new FormGroup({
+  email:new FormControl(),
+  pass:new FormControl()
+})
+msg:string=""
+loginDetails:Array<Login>=[]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+createAccount(){
+  let login = this.loginRef.value;
+  let l = {email:login.email,pass:login.pass};    //literal style object
+  let result = this.loginDetails.find(obj=>obj.email==l.email);
+  if(result==undefined){
+    this.loginDetails.push(l);
+    sessionStorage.setItem("loginInfo",JSON.stringify(this.loginDetails));
+this.msg = "Account Created Successfully"
+  }else{
+    this.msg="Email Id must be unique";
+  }
+  this.loginRef.reset();
+  
+}
+}
